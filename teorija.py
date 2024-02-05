c5808506884b8c730c8dcb9f5c332ae093ad8c61
irac45 <145848012+irac45@users.noreply.github.com>
2024-02-05 10:58:55 +0100

    teorija.py

---
+++ teorija.py
@@ -0,0 +1,7 @@
+Što su i zbog čega koristimo module u pythonu?
+
+U Pythonu, moduli su datoteke koje sadrže Python kod, 
+uključujući definicije funkcija, klasa i varijabli. 
+Moduli omogućuju organizaciju koda u logičke cjeline radi lakšeg upravljanja i održavanja projekta.
+Nekoliko razloga zašto koristimo module u Pythonu:
+Organizacija koda, Ponovno korištenje koda, Čitljivost koda, Imenovanje prostora, Podjela odgovornosti.
