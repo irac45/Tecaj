c7ba683d2e74dd5a3790cccd10a502da62770719
irac45 <145848012+irac45@users.noreply.github.com>
2024-02-09 13:48:06 +0100

    teorija.py

--- teorija.py
+++ teorija.py
@@ -1,7 +1,37 @@
 Što su i zbog čega koristimo module u pythonu?
+U Pythonu, moduli su datoteke koje sadrže Python kod,
+uključujući definicije funkcija, klasa i varijabli.
+Module koristimo zbog bolje organizacija koda, čitljivosti koda, imenovanje prostora...
+Što je klasa a što objekt?
+Klasa je korisnički definiran tip podatka kojim se modeliraju objekti sličnih svojstava.
+Klasa predstavlja predložak na temelju kojeg će se definirati varijable unutar koda.
+Objekt je stvarna instanca neke klase koja se nalazi u memoriji.
 
-U Pythonu, moduli su datoteke koje sadrže Python kod, 
-uključujući definicije funkcija, klasa i varijabli. 
-Moduli omogućuju organizaciju koda u logičke cjeline radi lakšeg upravljanja i održavanja projekta.
-Nekoliko razloga zašto koristimo module u Pythonu:
-Organizacija koda, Ponovno korištenje koda, Čitljivost koda, Imenovanje prostora, Podjela odgovornosti.
+Koja dva osnovna tipa baza podataka se danas koriste?
+Danas se najčešće koriste relacijske i NoSQl baze.
+
+Što je IoT?
+IoT je naziv za mrežu elektroničkih sustava povezanih putem interneta.
+Sustavi se sastoje od hardware-a i software-a
+
+Što je pandas?
+Pandas je alat za analiziranje podataka u pythonu a naziv je proizašao iz Panel Data.
+Podatke sprema u DataFrame
+
+Za što se koristi Matplotlib?
+Matplotlib koristimo za vizualizaciju i izradu grafova.
+
+
+Što od navedenog nije točno kada govorimo o računalnom razmišljanju?
+
+Računalno razmišljanje je najbolje za programe razvijene u Pythonu, ali može se koristiti i uz druge programske jezike.
+Računalno razmišljanje je primjenjivo samo na probleme za koje se mogu izraditi računalni programi.
+Računalno razmišljanje se može primjenjivati za rješavanje svakodnevnih problema.
+
+Računalno razmišljanje zahtijeva razmišljanje kao računalo.
+
+Koja je razlika između json.dump() i json.dumps() metoda? Objasnite kada koju metodu koristimo?
+
+json.dump() i json.dumps() su obje metode Python modula json koje se koriste za serijalizaciju Python objekata u JSON format. Međutim, razlikuju se u načinu na koji obrađuju podatke.
+
+json.dump() se koristi za pisanje JSON podataka u datoteku, dok se json.dumps() koristi za pretvaranje Python objekta u JSON string.
