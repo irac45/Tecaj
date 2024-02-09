90327344387544199d8b6058e3d58aac03860da1
irac45 <145848012+irac45@users.noreply.github.com>
2024-02-09 13:56:17 +0100

    DIO 2: kreirati argumente za klasu Movie.py

---
+++ DIO 2: kreirati argumente za klasu Movie.py
@@ -0,0 +1,12 @@
+DIO 2: kreirati argumente za klasu Movie"
+
+class Movie:
+def_init__(self, title, year):
+pass
+
+"""Rijesenje"""
+
+class Movie:
+def_init__(self, title, year):
+self.title = title
+self.year = year
